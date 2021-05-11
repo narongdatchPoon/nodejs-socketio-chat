6894216713735e2f7a67b741740723a5b0c4a93b
Narongdatch Poon <narongdatch.poon@gmail.com>
2021-05-12 00:21:08 +0700

    initial commit

---
+++ app.js
@@ -0,0 +1,36 @@
+const express = require('express')
+const app = express()
+const path = require('path')
+
+const APP_PORT = 5555
+
+
+const server = app.listen(APP_PORT, () => {
+  console.log(`App running on port ${APP_PORT}`)
+})
+
+const io = require('socket.io').listen(server)
+
+// // ตั้งค่า เพื่อให้ express ทำการ render view ที่โฟลเดอร์ views
+// // และใช้ template engine เป็น pug
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'pug')
+
+app.use(express.static('public'))
+
+app.get('/', (req, res) => {
+  res.render('index')
+})
+
+io.on('connection', (socket) => {
+  socket.on('chatter', function(message) {
+    console.log('message : ' + message);
+    io.emit('chatter', message)
+  });
+  console.log('a user connected')
+})
+
+
+//app.listen(APP_PORT, () => {
+//  console.log(`App running on port ${APP_PORT}`)
+//})
